e2c7f430097333039862d715c4bc79bc1addb605
Ivo Murrell <ivoisbelongtous@gmail.com>
2017-01-31 15:32:40 +0000

    Allow very simple debug view of parse tree.

--- interpreter.go
+++ interpreter.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"strings"
 	"text/scanner"
 )
@@ -9,6 +10,16 @@ import (
 
 func main() {
 	var s scanner.Scanner
-	s.Init(strings.NewReader("aaaa->P"))
+	s.Init(strings.NewReader("aaaa->Q|b->c->P"))
 	cspParse(&cspLex{s})
+
+	print_tree(root)
+}
+
+func print_tree(node *cspTree) {
+	if node != nil {
+		log.Printf("%p, %v", node, *node)
+		print_tree(node.left)
+		print_tree(node.right)
+	}
 }
